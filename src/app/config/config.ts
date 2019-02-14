6db2ef58d177bc3ebcc6ceb70d35db011dfecc41
seiit <cristian.gomez@rokk3rlabs.com>
2019-02-14 12:00:25 -0500

    change url

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,14 +1,14 @@
 
 export class Config {
-
+/*
 constructor(server = 'http://localhost:3000'){
   this.server = server;
   }
   server : string;
-}
-/*
+}*/
+
 constructor(server = 'https://vitacorapp.herokuapp.com'){
     this.server = server;
     }
     server : string;
-}*/
+}
